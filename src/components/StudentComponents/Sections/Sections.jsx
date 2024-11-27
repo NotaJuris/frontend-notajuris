8877693ff555c40547344d12eb3775cd94e50a6b
oluizsilva <luizzfs12@gmail.com>
2024-11-26 22:12:21 -0300

    initial integration of activities api

--- src/components/StudentComponents/Sections/Sections.jsx
+++ src/components/StudentComponents/Sections/Sections.jsx
@@ -1,32 +1,55 @@
-import './Sections.css'
-import Card from '../CardComponents/Card'; 
+import "./Sections.css";
+import Card from "../CardComponents/Card";
+import axios from 'axios';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 /* import plus from '../../../assets/images/plus.svg'; */
 
 
+async function getActivities() {
+  try {
+    
+    const headers = { Authorization: `Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcGktbm90YWp1cmlzIiwic3ViIjoiMiIsImlhdCI6MTczMjY2NzA2MywiZXhwIjoxNzMyNjc0MjYzfQ.9Sqw8ZpUpcA00VYx-nFP43mIeTNAEu_8tDcQPWjylZCs6mkIO5zR8Xn92MOVIqEeXsOJ9HlJ-sXgRCVtKczRBA` };
+    
+    const userActivities = await axios.get(
+      "http://capable-dream-production.up.railway.app/v1/atividades/2/usuario",
+      {
+        headers: headers,
+      }
+    );
+  
+  console.log(userActivities.data);
+
+  } catch (error) {
+    console.error("Erro: ", error);
+  }
+}
+
 function Sections() {
+  
+  getActivities();
+
   return (
-    <main className= "sections container"> 
+    <main className="sections container">
       <section className="left-section">
-      <div className= "button-container">
-      <span className= "activity-text">Enviar nova atividade</span>
-      <button className="new-activity-button"> {/*<img src={plus} alt="Plus" className="plus-icon"/> */} + </button>
-      </div>
+        <div className="button-container">
+          <span className="activity-text">Enviar nova atividade</span>
+          <button className="new-activity-button">
+            {" "}
+            {/*<img src={plus} alt="Plus" className="plus-icon"/> */} +{" "}
+          </button>
+        </div>
       </section>
 
-      <section className= "right-section">
+      <section className="right-section">
         <div className="card-container">
-          <h2 style={{ color: 'white'}}>Histórico de atividades</h2>
-          <Card title="Atendimento jurídico" date="23/09/2024" status="Recebida" />
-          <Card title="Audiência" date="12/04/2025" status="Pendente"/>
-          <Card title="Reunião" date="30/07/2025" status="Concluída"/>
-          <Card title="Audiência" date="20/10/2025" status="Pendente"/>
-          <Card title="Audiência" date="24/12/2025" status="Recebida"/>
-          <Card title="Audiência" date="20/10/2025" status="Pendente"/>
-          <Card title="Audiência" date="20/10/2025" status="Pendente"/>
-          <Card title="Audiência" date="20/10/2025" status="Pendente"/>
-          <Card title="Audiência" date="20/10/2025" status="Pendente"/>
-          <Card title="Audiência" date="20/10/2025" status="Pendente"/>
-          <Card title="Audiência" date="20/10/2025" status="Pendente"/>
+          <h2 style={{ color: "white" }}>Histórico de atividades</h2>
+
+          <Card
+            title="Atendimento jurídico"
+            date="23/09/2024"
+            status="Recebida"
+          />
         </div>
       </section>
     </main>
